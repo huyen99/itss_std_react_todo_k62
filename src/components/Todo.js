b9fafa3e322762b3575476704d1efdcb7c595f25
huyen99 <ec2-user@ip-172-31-0-110.ec2.internal>
2021-04-12 05:56:03 +0000

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /* 
   【Todoのデータ構成】
@@ -19,6 +20,7 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
+  const [filterItems, setFilterItems] = useState([]);
   const [items, putItems] = React.useState([
       /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
@@ -26,6 +28,10 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  
+    useEffect(() => {
+    setFilterItems(items);
+  }, [items]);
 
   const handleOnEnterInput = (newItem) => {
     putItems([
@@ -38,16 +44,29 @@ function Todo() {
     ])
   }
 
+  const handleOnFilterClick = (element) => {
+    if (element.id === 2) {
+      setFilterItems(items.filter((item) => item.done === false));
+    }
+    else if (element.id === 3) {
+      setFilterItems(items.filter((item) => item.done === true));
+    }
+    else {
+      setFilterItems(items);
+    }
+  }
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input onEnterInput={handleOnEnterInput} />
-      {items.map(item => (
+      <Filter onFilterClick={handleOnFilterClick}/>
+      {filterItems.map(item => (
       <TodoItem 
         key={item.key}
-        item={item}
+        item={item} 
       />
       ))}
       <div className="panel-block">
